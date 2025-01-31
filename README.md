0a13530c4ee9c30198ecced6729061de8af4e55d
chrzanof <58990661+chrzanof@users.noreply.github.com>
2025-01-31 20:03:01 +0100

    wiecej wykresow

--- README.md
+++ README.md
@@ -35,3 +35,86 @@ Eksploracja Danych
 
 
 ![Macierz korelacji](correlation_matrix.png)
+## Rozkład ciśnienia krwi
+
+
+![Rozkład ciśnienia krwi](histogram_trestbps.png)
+## Rozkład depresji ST
+
+
+![Rozkład depresji ST](histogram_oldpeak.png)
+## Poziom cholesterolu vs target
+
+
+![Poziom cholesterolu vs target](boxplot_target_chol.png)
+## Wiek vs Poziom Cholesterolu
+
+
+![Wiek vs Poziom Cholesterolu](scatter_age_chol_target.png)
+## Macierz kontyngencji dla sex i target
+
+
+|   sex |   0 |   1 |   2 |   3 |   4 |
+|------:|----:|----:|----:|----:|----:|
+|     0 |  72 |   9 |   7 |   7 |   2 |
+|     1 |  92 |  46 |  29 |  28 |  11 |
+## Macierz kontyngencji dla cp i target
+
+
+|   cp |   0 |   1 |   2 |   3 |   4 |
+|-----:|----:|----:|----:|----:|----:|
+|    1 |  16 |   5 |   1 |   0 |   1 |
+|    2 |  41 |   6 |   1 |   2 |   0 |
+|    3 |  68 |   9 |   4 |   4 |   1 |
+|    4 |  39 |  35 |  30 |  29 |  11 |
+## Macierz kontyngencji dla fbs i target
+
+
+|   fbs |   0 |   1 |   2 |   3 |   4 |
+|------:|----:|----:|----:|----:|----:|
+|     0 | 141 |  51 |  27 |  27 |  12 |
+|     1 |  23 |   4 |   9 |   8 |   1 |
+## Macierz kontyngencji dla restecg i target
+
+
+|   restecg |   0 |   1 |   2 |   3 |   4 |
+|----------:|----:|----:|----:|----:|----:|
+|         0 |  95 |  23 |  19 |  12 |   2 |
+|         1 |   1 |   0 |   1 |   1 |   1 |
+|         2 |  68 |  32 |  16 |  22 |  10 |
+## Macierz kontyngencji dla exang i target
+
+
+|   exang |   0 |   1 |   2 |   3 |   4 |
+|--------:|----:|----:|----:|----:|----:|
+|       0 | 141 |  30 |  14 |  12 |   7 |
+|       1 |  23 |  25 |  22 |  23 |   6 |
+## Macierz kontyngencji dla slope i target
+
+
+|   slope |   0 |   1 |   2 |   3 |   4 |
+|--------:|----:|----:|----:|----:|----:|
+|       1 | 106 |  22 |   7 |   6 |   1 |
+|       2 |  49 |  31 |  26 |  24 |  10 |
+|       3 |   9 |   2 |   3 |   5 |   2 |
+## Macierz kontyngencji dla ca i target
+
+
+|   ca |   0 |   1 |   2 |   3 |   4 |
+|-----:|----:|----:|----:|----:|----:|
+|    0 | 133 |  27 |   9 |   8 |   3 |
+|    1 |  21 |  19 |  14 |   8 |   3 |
+|    2 |   7 |   6 |   9 |  14 |   2 |
+|    3 |   3 |   3 |   4 |   5 |   5 |
+## Macierz kontyngencji dla thal i target
+
+
+|   thal |   0 |   1 |   2 |   3 |   4 |
+|-------:|----:|----:|----:|----:|----:|
+|      3 | 130 |  22 |   8 |   6 |   2 |
+|      6 |   6 |   3 |   6 |   1 |   2 |
+|      7 |  28 |  30 |  22 |  28 |   9 |
+## Analiza Outlierów
+
+
+![Analiza Outlierów](outliers.png)
--- main.py
+++ main.py
@@ -5,16 +5,23 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 from mdutils.mdutils import MdUtils
 
+# 1. Pobranie danych
+# Źródło: UCI Heart Disease Dataset
 dataset_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/heart-disease/processed.cleveland.data"
 columns = ["age", "sex", "cp", "trestbps", "chol", "fbs", "restecg", "thalach", "exang", "oldpeak", "slope", "ca", "thal", "target"]
 data = pd.read_csv(dataset_url, names=columns, na_values="?")
 
+# 2. Czyszczenie danych
+# Usuwanie duplikatów
 data = data.drop_duplicates()
 
+# Uzupełnianie brakujących wartości
 data.fillna(data.median(numeric_only=True), inplace=True)
 
+# Usunięcie niepotrzebnych kolumn (jeśli istnieją całkowicie puste)
 data = data.dropna(axis=1, how='all')
 
+# 3. Standaryzacja i normalizacja
 scaler = StandardScaler()
 minmax_scaler = MinMaxScaler()
 
@@ -26,12 +33,16 @@ data_standardized[numerical_cols] = scaler.fit_transform(data[numerical_cols])
 data_normalized = data.copy()
 data_normalized[numerical_cols] = minmax_scaler.fit_transform(data[numerical_cols])
 
+# 4. Eksploracja danych
+# Tworzenie obiektu raportu Markdown
 mdFile = MdUtils(file_name='README', title='Eksploracja Danych')
 
+# Dodawanie statystyk opisowych do raportu
 mdFile.new_header(level=1, title='Statystyki opisowe')
 desc_stats = data.describe().to_markdown()
 mdFile.new_paragraph(desc_stats)
 
+# Wykresy
 def save_and_add_plot(data, plot_func, filename, title, xlabel, ylabel):
     plt.figure(figsize=(12, 6))
     plot_func(data)
@@ -43,6 +54,7 @@ def save_and_add_plot(data, plot_func, filename, title, xlabel, ylabel):
     mdFile.new_header(level=2, title=title)
     mdFile.new_paragraph(f'![{title}]({filename})')
 
+# Histogram wieku pacjentów
 save_and_add_plot(
     data['age'],
     lambda d: sns.histplot(d, bins=20, kde=True),
@@ -51,6 +63,8 @@ save_and_add_plot(
     'Wiek',
     'Liczba pacjentów'
 )
+
+# Wykres pudełkowy poziomu cholesterolu
 save_and_add_plot(
     data['chol'],
     lambda d: sns.boxplot(x=d),
@@ -60,6 +74,7 @@ save_and_add_plot(
     ''
 )
 
+# Wykres rozrzutu: wiek vs maksymalne tętno
 save_and_add_plot(
     data,
     lambda d: sns.scatterplot(x=d['age'], y=d['thalach'], hue=d['sex']),
@@ -69,6 +84,7 @@ save_and_add_plot(
     'Maksymalne Tętno'
 )
 
+# Wykres rozrzutu: poziom cholesterolu vs ciśnienie krwi
 save_and_add_plot(
     data,
     lambda d: sns.scatterplot(x=d['chol'], y=d['trestbps'], hue=d['target']),
@@ -78,6 +94,7 @@ save_and_add_plot(
     'Ciśnienie Krwi'
 )
 
+# Macierz korelacji
 plt.figure(figsize=(12, 6))
 corr = data.corr(numeric_only=True)
 sns.heatmap(corr, annot=True, cmap="coolwarm")
@@ -87,9 +104,67 @@ plt.close()
 mdFile.new_header(level=2, title='Macierz korelacji')
 mdFile.new_paragraph('![Macierz korelacji](correlation_matrix.png)')
 
+# 5. Dodatkowe analizy
+# Histogramy dla innych zmiennych numerycznych
+save_and_add_plot(
+    data['trestbps'],
+    lambda d: sns.histplot(d, bins=20, kde=True),
+    'histogram_trestbps.png',
+    'Rozkład ciśnienia krwi',
+    'Ciśnienie Krwi',
+    'Liczba pacjentów'
+)
+
+save_and_add_plot(
+    data['oldpeak'],
+    lambda d: sns.histplot(d, bins=20, kde=True),
+    'histogram_oldpeak.png',
+    'Rozkład depresji ST',
+    'Depresja ST',
+    'Liczba pacjentów'
+)
+
+# Wykres pudełkowy dla zmiennych kategorycznych (np. 'target' vs 'chol')
+save_and_add_plot(
+    data,
+    lambda d: sns.boxplot(x=d['target'], y=d['chol']),
+    'boxplot_target_chol.png',
+    'Poziom cholesterolu vs target',
+    'Target',
+    'Poziom cholesterolu'
+)
+
+# Wykresy rozrzutu: Wiek vs cholesterol z uwzględnieniem 'target'
+save_and_add_plot(
+    data,
+    lambda d: sns.scatterplot(x=d['age'], y=d['chol'], hue=d['target']),
+    'scatter_age_chol_target.png',
+    'Wiek vs Poziom Cholesterolu',
+    'Wiek',
+    'Poziom Cholesterolu'
+)
+
+# Macierz kontyngencji dla zmiennych kategorycznych
+categorical_cols = ['sex', 'cp', 'fbs', 'restecg', 'exang', 'slope', 'ca', 'thal']
+for col in categorical_cols:
+    contingency_table = pd.crosstab(data[col], data['target'])
+    mdFile.new_header(level=2, title=f'Macierz kontyngencji dla {col} i target')
+    mdFile.new_paragraph(contingency_table.to_markdown())
+
+# Analiza outlierów
+plt.figure(figsize=(12, 6))
+sns.boxplot(data=data[numerical_cols])
+plt.title('Analiza outlierów dla zmiennych numerycznych')
+plt.savefig('outliers.png')
+plt.close()
+mdFile.new_header(level=2, title='Analiza Outlierów')
+mdFile.new_paragraph('![Analiza Outlierów](outliers.png)')
+
+# 6. Zapis przetworzonych danych
 data_standardized.to_csv("data_standardized.csv", index=False)
 data_normalized.to_csv("data_normalized.csv", index=False)
 
+# Zapis raportu do pliku Markdown
 mdFile.create_md_file()
 
 print("Proces eksploracji i obróbki danych zakończony. Pliki zapisane.")
